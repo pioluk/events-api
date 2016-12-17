4e641815e44301774183ad2edeb8e86e8ad1319b
Piotr Łukomiak <cieplak32@gmail.com>
2016-12-17 12:15:37 +0100

    use different config for development and production

--- config/config.js
+++ config/config.js
@@ -7,10 +7,10 @@ module.exports = {
     dialect: 'postgres'
   },
   development: {
-    username: process.env.RDS_USERNAME,
-    password: process.env.RDS_PASSWORD,
-    database: process.env.RDS_DB_NAME,
-    host: process.env.RDS_HOSTNAME,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
     dialect: 'postgres'
   },
   test: {
